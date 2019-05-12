827d507710f854b237e2a99513814bb5906530f1
Pavel <pavel.lance@gmail.com>
2019-05-12 20:03:45 +0300

    Разработан шорткод

--- wp-post-autocomplete.php
+++ wp-post-autocomplete.php
@@ -1,6 +1,36 @@
 <?php
 /*
 Plugin Name:  WP Post Autocomplete
-Description:  Яякс поиск статей с автокомплитом
+Description:  Яякс поиск статей с автокомплитом. Для отображения формы поиска, используйте шорткод [post-autocomplete-form']
 Version: 1.0.0
 */
+
+defined('ABSPATH') or die('No script kiddies please!');
+
+if(class_exists('WpPostAutocomplete') == false):
+	class WpPostAutocomplete
+	{
+		public function __construct()
+		{
+			add_shortcode('post-autocomplete-form', array($this, 'post_autocomplete_form'));
+		}
+
+		//===========================================================
+		// SHORTCODE
+		//===========================================================
+
+		/**
+		 * Отображает форму поиска
+		 */
+		public function post_autocomplete_form()
+		{
+			$content  = '<div class="wrap-post-autocomplete">';
+			$content .= '<input type="text" class="post-autocomplete-field" placeholder="Введите текст для поиска">';
+			$content .= '</div>';
+
+			return $content;
+		}
+	}
+
+	new WpPostAutocomplete();
+endif;
